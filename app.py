de1d51c7c7301bf7ac7f414ea491095432d5dabc
Justin Gosses <11600445+JustinGOSSES@users.noreply.github.com>
2023-05-28 00:07:46 -0500

    language

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+#    print('Request for index page received')
+#    return render_template('index.html')
+    print('Request for explore page received')
+    return render_template('explore.html')
 
 @app.route('/explore')
 def explore():
@@ -22,16 +24,16 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#    name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+#    if name:
+#        print('Request for hello page received with name=%s' % name)
+#        return render_template('hello.html', name = name)
+#    else:
+#        print('Request for hello page received with no name or blank name -- redirecting')
+#        return redirect(url_for('index'))
    
 @app.route('/fieldStopIntro', methods=['POST'])
 def fieldStopIntro():
